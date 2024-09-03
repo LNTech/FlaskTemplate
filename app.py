457dbb952f5cfad985422916f86eb7ac5841ad52
Luke Oliver <lo106@student.le.ac.uk>
2024-09-03 23:47:54 +0100

    add files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':    
+    app.run(debug=True)
